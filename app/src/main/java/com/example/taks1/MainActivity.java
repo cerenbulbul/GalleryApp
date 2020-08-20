5795c4b6ea0da8299940d1e231768c03f7cb4a5d
Ceren <cerenbulbul27@gmail.com>
2020-08-20 07:55:37 -0700

    Create an Activity that is List the Photos

--- app/src/main/java/com/example/taks1/MainActivity.java
+++ app/src/main/java/com/example/taks1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.taks1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -31,6 +32,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        LinearLayout line1 = findViewById(R.id.line1);
+        LinearLayout line2 = findViewById(R.id.line2);
+        LinearLayout line3 = findViewById(R.id.line3);
+        LinearLayout line4 = findViewById(R.id.line4);
+        LinearLayout line5 = findViewById(R.id.line5);
+        LinearLayout line6 = findViewById(R.id.line6);
+        LinearLayout line7 = findViewById(R.id.line7);
+        LinearLayout line8 = findViewById(R.id.line8);
+        LinearLayout line9 = findViewById(R.id.line9);
+        LinearLayout line10 = findViewById(R.id.line10);
+        LinearLayout line11 = findViewById(R.id.line11);
+        LinearLayout line12 = findViewById(R.id.line12);
+        LinearLayout line13 = findViewById(R.id.line13);
+        LinearLayout line14 = findViewById(R.id.line14);
+
+        line1.setOnClickListener((View.OnClickListener) this);
+
         androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.inflateMenu(R.menu.toolbar_menu);
         toolbar.setOnMenuItemClickListener(new androidx.appcompat.widget.Toolbar.OnMenuItemClickListener() {
@@ -50,6 +68,22 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.line1:
+                openActivity();
+                break;
+            case R.id.line2:
+                openActivity();
+                break;
+        }
+    }
+
+    public void openActivity(){
+        Intent intent = new Intent(this, listPhotos.class);
+        startActivity(intent);
     }
 
     @Override
