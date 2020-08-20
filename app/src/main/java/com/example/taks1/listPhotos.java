98641176de7f3707bc06f86b4761a26e859e03d6
Ceren <cerenbulbul27@gmail.com>
2020-08-20 08:05:28 -0700

    Create a GridView in ListPhotos Activity & Upload photos

--- app/src/main/java/com/example/taks1/listPhotos.java
+++ app/src/main/java/com/example/taks1/listPhotos.java
@@ -3,12 +3,18 @@ package com.example.taks1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class listPhotos extends AppCompatActivity {
+
+    GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_photos);
+
+        gridView = (GridView) findViewById(R.id.gridView);
+
     }
 }
